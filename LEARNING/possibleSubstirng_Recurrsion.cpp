bc2792c802a8c016ce3510d5186612dcf0f42de6
miyasajid19 <miyasajid19@gmail.com>
2024-08-03 14:46:03 +0530

    first commit

---
+++ LEARNING/possibleSubstirng_Recurrsion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void possiblesubstring(string str,string ans)
+{
+    if (str.length()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    char ch=str[0];
+    string remaining=str.substr(1);
+    possiblesubstring(remaining,ans);
+    possiblesubstring(remaining,ans+ch);
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    cout<<"enter string"<<endl;
+    string str;
+    cin>>str;
+    cout<<"possiible substrings are : "<< endl;
+    possiblesubstring(str,"");
+    return EXIT_SUCCESS;
+}
