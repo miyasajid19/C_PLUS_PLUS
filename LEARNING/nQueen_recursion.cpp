67666e72a27531740ff23aa6c0bd58adfc8a21e0
miyasajid19 <miyasajid19@gmail.com>
2024-08-04 10:05:47 +0530

    first commit

---
+++ LEARNING/nQueen_recursion.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+bool isSafe(int **arr, int x, int y, int n)
+{
+    // Check column
+    for (int row = 0; row < x; row++)
+    {
+        if (arr[row][y] == 1)
+        {
+            return false;
+        }
+    }
+
+    // Check left diagonal
+    int row = x;
+    int col = y;
+    while (row >= 0 && col >= 0)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col--;
+    }
+
+    // Check right diagonal
+    row = x;
+    col = y;
+    while (row >= 0 && col < n)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col++;
+    }
+
+    return true;
+}
+
+bool nQueen(int **arr, int x, int n)
+{
+    if (x >= n)
+    {
+        return true;
+    }
+
+    for (int col = 0; col < n; col++)
+    {
+        if (isSafe(arr, x, col, n))
+        {
+            arr[x][col] = 1;
+            if (nQueen(arr, x + 1, n))
+            {
+                return true;
+            }
+            arr[x][col] = 0; // Backtracking
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    cout << "Enter the dimension of the board: " << endl;
+    int n;
+    cin >> n;
+
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+    if (nQueen(arr, 0, n))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "Solution does not exist" << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
