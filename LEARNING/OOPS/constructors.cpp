e4373ee99e9c2393da0fe57a41c422829cdf7fc3
miyasajid19 <miyasajid19@gmail.com>
2024-08-05 19:58:18 +0530

    first commit

--- LEARNING/OOPS/constructors.cpp
+++ LEARNING/OOPS/constructors.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 using namespace std;
 class Students
 {
@@ -33,15 +34,43 @@ int main()
 #endif
     Students s("sajid miya", 20, 9.55);
     s.print();
-    
+
     // class constructed via default constructor if default is not defined as if we define the constructor then default constructor is  overwritten and so we have to redeclare it
     //  Students s;
     //  s.name="sajid miya";
     //  s.age=20;
     //  s.gpa=9.51;
-//however we can print it via both approaches
+    // however we can print it via both approaches
     print(s);
-    cout<<s.name<< " "<<s.age<<" "<< s.gpa<<endl;
+    cout << s.name << " " << s.age << " " << s.gpa << endl;
 
+    // array
+    int size;
+    float gpa;
+    string name;
+    int age;
+    cout << "how many students do you have?" << endl;
+    cin >> size;
+    // you can not define the array via this process you can use vector
+    // Students s[size];
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << "enter name, age, gpa  of student " << i + 1 << endl;
+    //     cin>>name>>age>>gpa;
+    //     s[i](name,age,gpa);
+    // }
+    vector<Students> student;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "enter name, age, gpa  of student " << i + 1 << endl;
+        cin >> name >> age >> gpa;
+        Students tempStudent(name, age, gpa);
+        student.push_back(tempStudent);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        student[i].print();
+    }
     return EXIT_SUCCESS;
 }
