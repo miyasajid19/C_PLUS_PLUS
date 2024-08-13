8a657d65cfc5db49c7c7e017ee331f6baca03d00
miyasajid19 <miyasajid19@gmail.com>
2024-08-14 00:45:34 +0530

    first commit

--- LEARNING/targetsum.cpp
+++ LEARNING/targetsum.cpp
@@ -7,7 +7,7 @@ int check(int arr[], int size, int targetSum)
     int count = 0;
     for (int i = 0; i < size; i++)
     {
-        for (int j = i; j < size; j++)
+        for (int j = i+1; j < size; j++)
         {
             if (arr[i] + arr[j] == targetSum)
             {
