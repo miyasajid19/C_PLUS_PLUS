ec18beb672285801425b25077d531c59329955f2
miyasajid19 <miyasajid19@gmail.com>
2024-08-14 00:48:36 +0530

    first commit

---
+++ LEARNING/triplets.cpp
@@ -0,0 +1,43 @@
+// program to count the total no of triplets that meets the sum
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int check(int arr[], int size, int targetSum)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i+1; j < size; j++)
+        {
+            for (int k=j+1;k<size;k++)
+            {
+                if (arr[i]+arr[j]+arr[k]==targetSum)
+                {
+                    cout << "index " << i << ", index " << j<<" and " <<k << endl;
+                      count++;    
+                }
+            }
+        }
+    }
+    return count;
+}
+int main()
+{
+    int *arr = nullptr;
+    cout << "enter how many data do you have" << endl;
+    int size;
+    cin >> size;
+    arr = new int[size]();
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter the element " << i + 1 << endl;
+        cin >> arr[i];
+    }
+    cout << "Enter the target sum :" << endl;
+    int targetsum;
+    cin >> targetsum;
+    int count=check(arr, size, targetsum);
+    cout << "Total possible triplets : " << count;
+delete[] arr;
+    return 0;
+}
