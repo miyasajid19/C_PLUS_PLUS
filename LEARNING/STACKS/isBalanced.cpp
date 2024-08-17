39b4399890c9a3bf3d67a82a8a2da3f8023556a3
miyasajid19 <miyasajid19@gmail.com>
2024-08-17 22:33:41 +0530

    this checks whether the given string is balanced or not

---
+++ LEARNING/STACKS/isBalanced.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <cstdlib>
+#include<string>
+using namespace std;
+
+class Stacks
+{
+    char *arr = nullptr;
+    int capacity;
+    int size;
+
+public:
+    Stacks(int capacity)
+    {
+        this->size = 0;
+        this->capacity = capacity;
+        arr = new char[capacity]();
+    }
+
+    void Push(char a)
+    {
+        if (size >= capacity)
+        {
+            cout << "stack overflow";
+            return;
+        }
+        arr[size++] = a;
+    }
+
+    void Pop()
+    {
+        if (size <= 0)
+        {
+            cout << "stack underflow";
+            return;
+        }
+        size--;
+    }
+
+    char Top()
+    {
+        if (size > 0) {
+            return arr[size - 1];
+        }
+        return '\0'; // Return null character if the stack is empty
+    }
+
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+
+    bool isFull()
+    {
+        return size == capacity;
+    }
+
+    int Size()
+    {
+        return size;
+    }
+
+    void erase()
+    {
+        this->size = 0;
+    }
+};
+
+bool isBalanced(string str)
+{
+    int len = str.length();
+    Stacks stack(len);
+
+    for (int i = 0; i < len; i++)
+    {
+        char a = str[i];
+        if (a == '[' || a == '{' || a == '(')
+        {
+            stack.Push(a);
+        }
+        else if (a == ']' && !stack.isEmpty() && stack.Top() == '[')
+        {
+            stack.Pop();
+        }
+        else if (a == '}' && !stack.isEmpty() && stack.Top() == '{')
+        {
+            stack.Pop();
+        }
+        else if (a == ')' && !stack.isEmpty() && stack.Top() == '(')
+        {
+            stack.Pop();
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return stack.isEmpty();
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("..input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+
+    string testCases[] = {
+        "{}{}{}(((())))",
+        "{[()]}",
+        "{[(])}",
+        "(((",
+        "[{()}]",
+        "[(])",
+        "{[()()]}",
+        "{",
+        "}",
+        ""
+    };
+
+    for (const string &testCase : testCases)
+    {
+        cout << "Checking string: \"" << testCase << "\"\n";
+        if (isBalanced(testCase))
+        {
+            cout << "Result: Balanced\n\n";
+        }
+        else
+        {
+            cout << "Result: Not Balanced\n\n";
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
