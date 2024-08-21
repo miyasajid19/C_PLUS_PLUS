4ac1ef65002c4ecffbbdf0e398006b5209ee949a
miyasajid19 <miyasajid19@gmail.com>
2024-08-21 21:04:02 +0530

    updated

--- LEARNING/LISTS/CIRCULAR LINKEDLISTS/lists.cpp
+++ LEARNING/LISTS/CIRCULAR LINKEDLISTS/lists.cpp
@@ -4,85 +4,106 @@
 
 using namespace std;
 
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("../../input.txt", "r", stdin);
-    freopen("../../output.txt", "w", stdout);
-#endif
-
+int main() {
+    // Initialize list1 with values
     list<int> list1 = {1, 2, 3, 4, 5};
 
-    // Using normal iterator
+    // Using a normal iterator to access elements
     auto itr = list1.begin();
     cout << *itr << endl; // Output: 1
 
-    // Advance the iterator correctly
-    advance(itr, 2);      // Advance by 2 positions
+    // Advance the iterator by 2 positions
+    advance(itr, 2);
     cout << *itr << endl; // Output: 3
 
-    // Using reverse iterator
+    // Using a reverse iterator to access elements
     auto ritr = list1.rbegin();
     cout << *ritr << endl; // Output: 5
 
-    // Advance the reverse iterator correctly
-    ritr += 2;            // Advance by 2 positions
+    // Advance the reverse iterator by 2 positions
+    advance(ritr, -2);
     cout << *ritr << endl; // Output: 3
 
-    // Traversal using ranged based loop
-    for (auto val : list1)
-    {
+    // Traversal using a range-based for loop
+    cout << "List1 elements: ";
+    for (auto val : list1) {
         cout << val << " ";
     }
     cout << endl;
 
-    // Using iterators
-    for (auto itr1 = list1.begin(); itr1 != list1.end(); ++itr1)
-    {
+    // Traversal using iterators
+    cout << "List1 elements (iterator): ";
+    for (auto itr1 = list1.begin(); itr1 != list1.end(); ++itr1) {
         cout << *itr1 << " ";
     }
     cout << endl;
 
     // Reverse traversal using reverse iterators
-    for (auto itr1 = list1.rbegin(); itr1 != list1.rend(); ++itr1)
-    {
+    cout << "List1 elements (reverse iterator): ";
+    for (auto itr1 = list1.rbegin(); itr1 != list1.rend(); ++itr1) {
         cout << *itr1 << " ";
     }
     cout << endl;
 
-    // Inserting elements into the list
+    // Inserting elements into list2
     list<int> list2 = {1, 2, 4, 5};
+    cout << "Initial list2 elements: ";
+    for (auto val : list2) {
+        cout << val << " ";
+    }
+    cout << endl;
+
     auto itr2 = list2.begin();
-    for (auto val : list2)
-    {
+    advance(itr2, 2); // Move iterator to position where 3 will be inserted
+    list2.insert(itr2, 3); // Insert 3 at the position
+
+    cout << "List2 after insertion: ";
+    for (auto val : list2) {
         cout << val << " ";
     }
     cout << endl;
 
-    advance(itr2, 2);
-    list2.insert(itr2, 3);
-    for (auto val : list2)
-    {
+    // Inserting multiple elements at the end
+    list2.insert(list2.end(), 2, 432); // Insert 432 twice
+    cout << "List2 after inserting 432 twice: ";
+    for (auto val : list2) {
         cout << val << " ";
     }
     cout << endl;
 
-    // Inserting the value count number of times
-    list2.insert(list2.end(), 2, 432);
-    for (auto val : list2)
-    {
-        cout << val << " ";
-    }
-    cout << endl;
-
-    // Inserting a range of elements from list2 into list2 itself (fixed)
+    // Inserting a range of elements from list2 into list2 itself
     auto itr3 = list2.begin();
     auto itr4 = itr2;
-    advance(itr4, 4); // Advance __itr to include the first 4 elements of list2
-    list2.insert(list2.begin(), itr3, itr4); // Insert the range at the beginning of list2
+    advance(itr4, 4); // Move itr4 to include the first 4 elements of list2
 
-    for (auto val : list2)
-    {
+    // Insert elements from [itr3, itr4) at the beginning of list2
+    list2.insert(list2.begin(), itr3, itr4);
+
+    cout << "List2 after inserting range: ";
+    for (auto val : list2) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Deleting a single element
+    itr3 = list2.begin();
+    advance(itr3, 2); // Move iterator to the element to be deleted
+    list2.erase(itr3);
+
+    cout << "List2 after deleting one element: ";
+    for (auto val : list2) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Deleting a range of elements
+    itr3 = list2.begin();
+    advance(itr3, 4); // Move itr2 to the starting point of the range to be deleted
+    advance(itr4, 6); // Move itr4 to the end point of the range to be deleted
+    list2.erase(itr3, itr4); // Delete elements in the range [itr3, itr4)
+
+    cout << "List2 after deleting range: ";
+    for (auto val : list2) {
         cout << val << " ";
     }
     cout << endl;
