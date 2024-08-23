2bab7689b7d945352928bc55c6bb06d440e3c12d
miyasajid19 <miyasajid19@gmail.com>
2024-08-23 16:04:39 +0530

    application of euclid's algorithms to find gcd of two numbers

--- LEARNING/RECURSION/GCD.cpp
+++ LEARNING/RECURSION/GCD.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
-#include<cstdlib>
+#include <cstdlib>
 using namespace std;
 int calcualteGCDbyDivision(int a, int b)
 {
-    if(b==0)
+    if (b == 0)
     {
         return a;
     }
-    
-    if(a>b)
+
+    if (a > b)
     {
-        swap(a,b);
+        swap(a, b);
     }
-    return calcualteGCDbyDivision(a,b%a);
+    return calcualteGCDbyDivision(a, b % a);
 }
 int calcualteGCDbyDifference(int a, int b)
 {
-    if(b==0)
+    if (b == 0)
     {
         return a;
     }
-    
-    if(a>b)
+
+    if (a > b)
     {
-        swap(a,b);
+        swap(a, b);
     }
-    return calcualteGCDbyDifference(a,b-a);
+    return calcualteGCDbyDifference(a, b - a);
 }
 int main()
 {
@@ -33,12 +33,12 @@ int main()
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
-int a,b;
-cout<<"enter the first number : "<<endl;
-cin>>a;
-cout<<"enter the first number : "<<endl;
-cin>>b;
-cout<<"The HCF/GCD is "<<calcualteGCDbyDifference( a,b)<<"  calculated by euclid's differene algorithm"<<endl;
-cout<<"The HCF/GCD is "<<calcualteGCDbyDivision( a,b)<<"  calcualted by euclid's division algorithm"<<endl;
+    int a, b;
+    cout << "enter the first number : " << endl;
+    cin >> a;
+    cout << "enter the first number : " << endl;
+    cin >> b;
+    cout << "The HCF/GCD is " << calcualteGCDbyDifference(a, b) << "  calculated by euclid's differene algorithm" << endl;
+    cout << "The HCF/GCD is " << calcualteGCDbyDivision(a, b) << "  calcualted by euclid's division algorithm" << endl;
     return EXIT_SUCCESS;
 }
