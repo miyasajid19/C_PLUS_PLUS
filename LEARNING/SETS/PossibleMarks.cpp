97ab55dcb36901a7662347a113f3a0fe89d105b1
miyasajid19 <miyasajid19@gmail.com>
2024-08-24 15:21:39 +0530

    find all the possible marks that a student can score

---
+++ LEARNING/SETS/PossibleMarks.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <set>
+using namespace std;
+set<int> possibily(int n, int p, int q)
+{
+    set<int> ans;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            int unattempted = n - (i + j);
+            if (unattempted >= 0)
+            {
+                ans.insert(i * p - j * q);
+            }
+            else
+            {
+                break;
+                ;
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+    int n, p, q;
+    cout << "Total no of questions :: " << endl;
+    cin >> n;
+    cout << "how much do i get i  made correct :: " << endl;
+    cin >> p;
+    cout << "how much i get if i did wrong :: " << endl;
+    cin >> q;
+    set<int> ans = possibily(n, p, q);
+    cout << "the possibilities are : " << endl;
+    for (auto value : ans)
+    {
+        cout << value << " ";
+    }
+    return EXIT_SUCCESS;
+}
