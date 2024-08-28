fb8562174d8377610e1f3b1cf4bc1d40a6cb3876
miyasajid19 <miyasajid19@gmail.com>
2024-08-29 03:26:35 +0530

    solution to problem 321

--- LEARNING/LEETCODE/problem_321.cpp
+++ LEARNING/LEETCODE/problem_321.cpp
@@ -1,18 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPowerOfTwo(int n) {
-        if (n <= 0) {
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+        {
             return false;
         }
-        if (n == 1) {
+        if (n == 1)
+        {
             return true;
         }
-        while (n != 0) {
-            if (n % 2 == 1) {
+        while (n != 0)
+        {
+            if (n % 2 == 1)
+            {
                 return false;
             }
             n /= 2;
-            if (n < 9 and n == 1) {
+            if (n < 9 and n == 1)
+            {
                 return true;
             }
         }
