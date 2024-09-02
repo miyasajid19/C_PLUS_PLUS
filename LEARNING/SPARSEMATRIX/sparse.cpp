436b813fbebe3707320e3eaa2889fda417dde5b2
miyasajid19 <miyasajid19@gmail.com>
2024-09-02 22:02:11 +0530

    sparse matrix template

--- LEARNING/SPARSEMATRIX/sparse.cpp
+++ LEARNING/SPARSEMATRIX/sparse.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+
 class Sparse
 {
     int **arr;
@@ -9,6 +10,7 @@ class Sparse
     int rows;
     int columns;
     int nonZeros;
+    
     int countNonZeros()
     {
         int count = 0;
@@ -33,8 +35,9 @@ public:
         {
             arr[i] = new int[columns];
         }
-        cout << "template created" << endl;
+        cout << "Template created" << endl;
     }
+
     void setMatrix()
     {
         for (int i = 0; i < this->rows; i++)
@@ -46,6 +49,7 @@ public:
             }
         }
     }
+
     void sparseit()
     {
         this->nonZeros = countNonZeros();
@@ -57,6 +61,7 @@ public:
         sparseMatrix[0][0] = this->rows;
         sparseMatrix[0][1] = this->columns;
         sparseMatrix[0][2] = this->nonZeros;
+
         int k = 1;
         for (int i = 0; i < this->rows; i++)
         {
@@ -71,13 +76,14 @@ public:
                 }
             }
         }
-        cout << "sparse matrix created" << endl;
+        cout << "Sparse matrix created" << endl;
     }
+
     void displaySparse()
     {
         if (sparseMatrix == nullptr)
         {
-            cout << "transpose matrix not created ";
+            cout << "Sparse matrix not created" << endl;
             return;
         }
         cout << "rows\tcolumns\tvalues\n";
@@ -86,61 +92,60 @@ public:
             cout << sparseMatrix[i][0] << "\t" << sparseMatrix[i][1] << "\t" << sparseMatrix[i][2] << endl;
         }
     }
+
     void RowMajorSort()
     {
         for (int i = 1; i <= this->nonZeros; i++)
         {
             for (int j = i + 1; j <= nonZeros; j++)
             {
-                if (transposedSparseMatrix[i][0] > transposedSparseMatrix[j][0] || (transposedSparseMatrix[i][0] == transposedSparseMatrix[j][0] && transposedSparseMatrix[i][1] > transposedSparseMatrix[j][1]))
+                if (transposedSparseMatrix[i][0] > transposedSparseMatrix[j][0] || 
+                    (transposedSparseMatrix[i][0] == transposedSparseMatrix[j][0] && transposedSparseMatrix[i][1] > transposedSparseMatrix[j][1]))
                 {
                     swap(transposedSparseMatrix[i], transposedSparseMatrix[j]);
                 }
             }
         }
     }
+
     void Transpose()
     {
-        this->nonZeros = countNonZeros();
         transposedSparseMatrix = new int *[this->nonZeros + 1];
         for (int i = 0; i <= this->nonZeros; i++)
         {
             transposedSparseMatrix[i] = new int[3];
         }
-        transposedSparseMatrix[0][0] = this->rows;
-        transposedSparseMatrix[0][1] = this->columns;
+        transposedSparseMatrix[0][0] = this->columns;  // rows and columns swapped for transpose
+        transposedSparseMatrix[0][1] = this->rows;
         transposedSparseMatrix[0][2] = this->nonZeros;
+
         int k = 1;
-        for (int i = 0; i < this->rows; i++)
+        for (int i = 1; i <= this->nonZeros; i++)
         {
-            for (int j = 0; j < this->columns; j++)
-            {
-                if (arr[i][j] != 0)
-                {
-                    transposedSparseMatrix[k][0] = j;
-                    transposedSparseMatrix[k][1] = i;
-                    transposedSparseMatrix[k][2] = arr[i][j];
-                    k++;
-                }
-            }
+            transposedSparseMatrix[k][0] = sparseMatrix[i][1];
+            transposedSparseMatrix[k][1] = sparseMatrix[i][0];
+            transposedSparseMatrix[k][2] = sparseMatrix[i][2];
+            k++;
         }
         RowMajorSort();
-        cout << " transpose of sparse matrix created" << endl;
+        cout << "Transpose of sparse matrix created" << endl;
     }
+
     void displayTransposedSparse()
     {
         if (transposedSparseMatrix == nullptr)
         {
-            cout << "transpose matrix not created ";
+            cout << "Transpose matrix not created" << endl;
             return;
         }
         cout << "rows\tcolumns\tvalues\n";
         for (int i = 0; i <= this->nonZeros; i++)
         {
-            cout << sparseMatrix[i][0] << "\t" << sparseMatrix[i][1] << "\t" << sparseMatrix[i][2] << endl;
+            cout << transposedSparseMatrix[i][0] << "\t" << transposedSparseMatrix[i][1] << "\t" << transposedSparseMatrix[i][2] << endl;
         }
     }
 };
+
 int main()
 {
     #ifndef ONLINE_JUDGE
