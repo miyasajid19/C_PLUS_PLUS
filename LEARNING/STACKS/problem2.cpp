413c01e1e0fadef80a51ece2b2d7ba5ff471b5f8
miyasajid19 <miyasajid19@gmail.com>
2024-09-06 23:43:30 +0530

    first commit

---
+++ LEARNING/STACKS/problem2.cpp
@@ -0,0 +1,29 @@
+// Find the minimum number of brackets that we need to remove
+// to make the given bracket sequence balanced.
+#include <iostream>
+#include <cstdlib>
+#include <stack>
+#include <string>
+using namespace std;
+class Stacks
+{
+    char *arr;
+    string str;
+
+public:
+    Stacks(string str, int size)
+    {
+        arr = new char[size];
+        this->str = str;
+    }
+    
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../../input.txt", "r", stdin);
+    freopen("../../output.txt", "w", stdout);
+#endif
+
+    return EXIT_SUCCESS;
+}
