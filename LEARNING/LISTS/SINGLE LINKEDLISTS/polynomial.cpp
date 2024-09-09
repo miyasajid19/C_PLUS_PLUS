c96f7816fdff0d65d9256f76e3fc701356bc843e
miyasajid19 <miyasajid19@gmail.com>
2024-09-09 19:03:49 +0530

    add insertions

---
+++ LEARNING/LISTS/SINGLE LINKEDLISTS/polynomial.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class Node
+{
+public:
+    int coefficient;
+    int index;
+    Node *Next;
+    Node(int coefficient, int index)
+    {
+        this->coefficient = coefficient;
+        this->index = index;
+        this->Next = nullptr;
+    }
+};
+class LinkedList
+{
+    Node *head;
+    LinkedList()
+    {
+        this->head = nullptr;
+    }
+    void InsertAtHead(int coefficient, int index)
+    {
+        Node *new_node = new Node(coefficient, index);
+        new_node->Next = head;
+        head = new_node;
+    }
+    void InsertAtTail(int coefficient, int index)
+    {
+        Node *new_node = new Node(coefficient, index);
+        if (head == nullptr)
+        {
+            head = new_node;
+            return;
+        }
+        Node *temp = head;
+        while (temp->Next != nullptr)
+        {
+            temp = temp->Next;
+        }
+        temp->Next = new_node;
+    }
+    void Insert(int coefficient, int power, int index)
+    {
+        if (index == 0)
+        {
+            InsertAtHead(coefficient, power);
+            return;
+        }
+        Node *temp = head;
+        int counter = 0;
+        Node* new_node=new Node(coefficient, power);
+        while (temp != nullptr and counter < index)
+        {
+            temp = temp->Next;
+            counter++;
+        }
+        if(temp=nullptr)
+        {
+            cout<<"Index out of bound"<<endl;
+            delete new_node;
+            return;
+        }
+    }
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("../../input.txt", "r", stdin);
+    freopen("../../output.txt", "w", stdout);
+#endif
+    cout << "all i want was the love and what i get is the pain" << endl;
+    return EXIT_SUCCESS;
+}
