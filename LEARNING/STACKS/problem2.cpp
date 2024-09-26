1eb4f3ef570b976a9dbc66ff0712dbfbe1b31a55
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-26 08:26:30 +0530

    no of brackets required to balance the parenthesis

--- LEARNING/STACKS/problem2.cpp
+++ LEARNING/STACKS/problem2.cpp
@@ -1,29 +1,62 @@
-// Find the minimum number of brackets that we need to remove
-// to make the given bracket sequence balanced.
 #include <iostream>
 #include <cstdlib>
 #include <stack>
 #include <string>
 using namespace std;
+
 class Stacks
 {
-    char *arr;
     string str;
 
 public:
-    Stacks(string str, int size)
+    Stacks(string str)
     {
-        arr = new char[size];
         this->str = str;
     }
-    
+
+    int minRemovals()
+    {
+        stack<char> s;
+        int removals = 0;
+
+        for (char ch : str)
+        {
+            if (ch == '(')
+            {
+                s.push(ch);
+            }
+            else if (ch == ')')
+            {
+                if (!s.empty() && s.top() == '(')
+                {
+                    s.pop(); // Matching pair found
+                }
+                else
+                {
+                    removals++; // Extra closing bracket
+                }
+            }
+        }
+
+        // Remaining opening brackets in stack are unmatched
+        removals += s.size();
+
+        return removals;
+    }
 };
+
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("../../input.txt", "r", stdin);
     freopen("../../output.txt", "w", stdout);
 #endif
+
+    string inputStr;
+    cin >> inputStr;
+
+    Stacks s(inputStr);
+    cout << "Minimum removals to balance: " << s.minRemovals() << endl;
 
     return EXIT_SUCCESS;
 }
