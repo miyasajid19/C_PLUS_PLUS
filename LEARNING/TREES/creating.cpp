a37ab4f837e826ba652017f88569920280429aa3
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-18 20:00:05 +0530

    building tree

---
+++ LEARNING/TREES/creating.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+class Tree
+{
+    Node *root;
+
+    Node *Build(Node *temp)
+    {
+        int value;
+        cout << "Enter value (-1 to terminate): ";
+        cin >> value;
+
+        if (value == -1)
+        {
+            return nullptr;
+        }
+
+        temp = new Node(value);
+
+        cout << "Enter left node for " << value << endl;
+        temp->left = Build(temp->left);
+
+        cout << "Enter right node for " << value << endl;
+        temp->right = Build(temp->right);
+
+        return temp;
+    }
+
+public:
+    Tree()
+    {
+        root = nullptr;
+    }
+
+    void BuildTree()
+    {
+        root = Build(root);
+        cout << "created";
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    Tree tree;
+    tree.BuildTree();
+    cout << endl;
+    return EXIT_SUCCESS;
+}
