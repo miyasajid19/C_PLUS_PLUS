18e41b0e1c082f36505317b461ff0ac5cc33bc4d
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-30 11:57:32 +0530

    Zig Zag Traversal of tree

--- LEARNING/TREES/ZigZagTraversal.cpp
+++ LEARNING/TREES/ZigZagTraversal.cpp
@@ -130,7 +130,7 @@ class Tree
             Traverse_Left(temp->right, ans);
         }
     }
-    void Traverse_left(Node *temp, vector<int> &ans)
+    void Traverse_Leaf(Node *temp, vector<int> &ans)
     {
         if (temp == nullptr)
             return;
@@ -138,8 +138,8 @@ class Tree
         if (temp->left == nullptr and temp->right == nullptr)
             ans.push_back(temp->value);
 
-        Traverse_left(temp->left, ans);
-        Traverse_left(temp->right, ans);
+        Traverse_Leaf(temp->left, ans);
+        Traverse_Leaf(temp->right, ans);
     }
     void Traverse_Right(Node *temp, vector<int> &ans)
     {
@@ -166,9 +166,9 @@ class Tree
         // traverse left nodes
         Traverse_Left(temp->left, ans);
 
-        // traverse left node
-        Traverse_left(temp->left, ans);
-        Traverse_left(temp->right, ans);
+        // traverse leaf node
+        Traverse_Leaf(temp->left, ans);
+        Traverse_Leaf(temp->right, ans);
 
         // traverse reverse wise right node
         Traverse_Right(temp->right, ans);
