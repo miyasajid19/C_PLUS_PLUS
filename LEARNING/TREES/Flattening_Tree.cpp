ca8b481707bc954cdac3fd18e70a01709f8a2170
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-31 16:50:52 +0530

    flattening of tree

---
+++ LEARNING/TREES/Flattening_Tree.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+class Tree
+{
+    Node *root;
+
+    Node *Build(Node *temp)
+    {
+        int value;
+        cout << "Enter value (-1 to terminate): ";
+        cin >> value;
+
+        if (value == -1)
+        {
+            return nullptr;
+        }
+
+        temp = new Node(value);
+
+        cout << "Enter left node for " << value << endl;
+        temp->left = Build(temp->left);
+
+        cout << "Enter right node for " << value << endl;
+        temp->right = Build(temp->right);
+
+        return temp;
+    }
+
+    void levelOrderTraversal(Node *temp)
+    {
+        if (temp == nullptr)
+            return;
+
+        queue<Node *> q;
+        q.push(temp);
+        q.push(nullptr); // To mark the end of a level
+
+        while (!q.empty())
+        {
+            Node *temp1 = q.front();
+            q.pop();
+
+            if (temp1 == nullptr)
+            {
+                cout << endl;
+                if (!q.empty())
+                    q.push(nullptr);
+            }
+            else
+            {
+                cout << temp1->value << "\t";
+                if (temp1->left)
+                    q.push(temp1->left);
+                if (temp1->right)
+                    q.push(temp1->right);
+            }
+        }
+    }
+
+public:
+    Tree()
+    {
+        root = nullptr;
+    }
+
+    void BuildTree()
+    {
+        root = Build(root);
+    }
+
+    void LevelOrderDisplay()
+    {
+        levelOrderTraversal(root);
+    }
+    // Flattening function
+    void FlattenTree()
+    {
+        Node *currentNode = root;
+        while (currentNode != nullptr)
+        {
+            if (currentNode->left)
+            {
+                // Find the rightmost node in the left subtree
+                Node *predecessor = currentNode->left;
+                while (predecessor->right)
+                {
+                    predecessor = predecessor->right;
+                }
+
+                // Reconfigure links to flatten the tree
+                predecessor->right = currentNode->right;
+                currentNode->right = currentNode->left;
+                currentNode->left = nullptr;
+            }
+            // Move to the right in the flattened structure
+            currentNode = currentNode->right;
+        }
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << boolalpha;
+    Tree tree;
+    tree.BuildTree();
+    cout << endl;
+    tree.LevelOrderDisplay();
+    tree.FlattenTree();
+    cout << "Flattened Tree : " << endl;
+    tree.LevelOrderDisplay();
+    return EXIT_SUCCESS;
+}
