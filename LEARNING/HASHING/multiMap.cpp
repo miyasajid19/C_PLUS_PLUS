a85f9e2e75e0e09cfb412c6609907587990e39e4
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-03 12:47:38 +0530

    multimap

--- LEARNING/HASHING/multiMap.cpp
+++ LEARNING/HASHING/multiMap.cpp
@@ -3,7 +3,8 @@
 #include <map>
 using namespace std;
 
-int main() {
+int main()
+{
     multimap<string, int> directory;
     directory.insert(make_pair("Uchiha Obito", 432));
     directory.insert(make_pair("Uchiha Obito", 1234)); // Inserts duplicate key with a different value
@@ -17,7 +18,8 @@ int main() {
 
     // Displaying the directory
     cout << "Original iteration:" << endl;
-    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it) {
+    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it)
+    {
         cout << "Name : " << it->first << " Contact No. : " << it->second << endl;
     }
     cout << endl;
@@ -31,7 +33,8 @@ int main() {
     // Deleting an element by key (removes all matching keys)
     directory.erase("Nohara Rin");
     cout << "After deleting 'Nohara Rin':" << endl;
-    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it) {
+    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it)
+    {
         cout << "Name : " << it->first << " Contact No. : " << it->second << endl;
     }
     cout << endl;
@@ -39,11 +42,13 @@ int main() {
     // Deleting the third element from the start
     multimap<string, int>::iterator itr = directory.begin();
     advance(itr, 3);
-    if (itr != directory.end()) {
+    if (itr != directory.end())
+    {
         directory.erase(itr);
     }
     cout << "After deleting the third element:" << endl;
-    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it) {
+    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it)
+    {
         cout << "Name : " << it->first << " Contact No. : " << it->second << endl;
     }
     cout << endl;
@@ -53,34 +58,49 @@ int main() {
     multimap<string, int>::iterator itr2 = directory.begin();
     advance(itr1, 1);
     advance(itr2, directory.size() - 1);
-    if (itr1 != directory.end() && itr2 != directory.end()) {
+    if (itr1 != directory.end() && itr2 != directory.end())
+    {
         directory.erase(itr1, itr2);
     }
     cout << "After erasing a range:" << endl;
-    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it) {
+    for (multimap<string, int>::iterator it = directory.begin(); it != directory.end(); ++it)
+    {
         cout << "Name : " << it->first << " Contact No. : " << it->second << endl;
     }
     cout << endl;
 
     // Finding values
     itr = directory.find("Uchiha Obito");
-    if (itr != directory.end()) {
+    if (itr != directory.end())
+    {
         cout << "Found :: " << itr->second << endl;
-    } else {
+    }
+    else
+    {
         cout << "Not Found" << endl;
     }
 
     itr = directory.find("Huguya Hinata");
-    if (itr != directory.end()) {
+    if (itr != directory.end())
+    {
         cout << "Found :: " << itr->second << endl;
-    } else {
+    }
+    else
+    {
         cout << "Not Found" << endl;
     }
 
+    // Checking if a key is present
+    cout << boolalpha;
+    found = directory.count("Hatake Kakashi");
+    cout << "Kakashi ??? " << found << endl;
+    found = directory.count("Uchiha Obito");
+    cout << "Obito ??? " << found << endl;
 
     // Reverse iteration
     cout << "Reverse iteration:" << endl;
-    for (multimap<string, int>::reverse_iterator rit = directory.rbegin(); rit != directory.rend(); ++rit) {
+    for (multimap<string, int>::reverse_iterator rit = directory.rbegin(); rit != directory.rend(); ++rit)
+    {
         cout << "Name : " << rit->first << " Contact No. : " << rit->second << endl;
     }
 
