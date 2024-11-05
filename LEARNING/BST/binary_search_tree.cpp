e3cbdf53122d09c05c89add9081d3bad0f1bfa12
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-05 07:53:19 +0530

    Searching of the node

--- LEARNING/BST/binary_search_tree.cpp
+++ LEARNING/BST/binary_search_tree.cpp
@@ -38,6 +38,36 @@ class BST
         return temp;
     }
 
+    void inOrderTraversal(Node *temp)
+    {
+        if (temp == nullptr)
+        {
+            return;
+        }
+        inOrderTraversal(temp->left);
+        cout << temp->value << "\t";
+        inOrderTraversal(temp->right);
+    }
+    void preOrderTraversal(Node *temp)
+    {
+        if (temp == nullptr)
+        {
+            return;
+        }
+        cout << temp->value << "\t";
+        preOrderTraversal(temp->left);
+        preOrderTraversal(temp->right);
+    }
+    void postOrderTraversal(Node *temp)
+    {
+        if (temp == nullptr)
+        {
+            return;
+        }
+        postOrderTraversal(temp->left);
+        postOrderTraversal(temp->right);
+        cout << temp->value << "\t";
+    }
     void levelOrderTraversal(Node *temp)
     {
         if (temp == nullptr)
@@ -68,6 +98,19 @@ class BST
             }
         }
     }
+    Node *searchTree(Node *temp, int value)
+    {
+        if (temp == nullptr)
+            return nullptr;
+
+        if (temp->value == value)
+            return temp;
+
+        if (value > temp->value)
+            return searchTree(temp->left, value);
+        else
+            return searchTree(temp->right, value);
+    }
 
 public:
     BST()
@@ -78,19 +121,34 @@ public:
     void BuildTree()
     {
         int value;
-        cout<<"Enter Element "<<endl;
-        cin>>value;
+        cout << "Enter Element " << endl;
+        cin >> value;
         while (value != -1)
         {
             root = BuildBST(root, value);
             cout << "Enter Element :: " << endl;
             cin >> value;
-        } 
+        } // O(log n)
     }
-
+    void InorderTraversal()
+    {
+        inOrderTraversal(root);
+    }
+    void PreOrderTraversal()
+    {
+        preOrderTraversal(root);
+    }
+    void PostOrderTraversal()
+    {
+        postOrderTraversal(root);
+    }
     void LevelOrderDisplay()
     {
         levelOrderTraversal(root);
+    }
+    bool SearchNode(int value)
+    {
+        return searchTree(root,value);
     }
 };
 
@@ -103,5 +161,18 @@ int main()
     BST bst;
     bst.BuildTree();
     bst.LevelOrderDisplay();
+    cout << endl;
+    cout << "Inorder Traversal :: ";
+    bst.InorderTraversal();
+    cout << endl;
+    cout << "PreOrder Traversal :: ";
+    bst.PreOrderTraversal();
+    cout << endl;
+    cout << "PostOrder Traversal :: ";
+    bst.PostOrderTraversal();
+ cout<<boolalpha;
+ cout<<endl;
+ cout<<"10??"<<bst.SearchNode(10)<<endl;
+ cout<<"432??"<<bst.SearchNode(432)<<endl;
     return EXIT_SUCCESS;
 }
