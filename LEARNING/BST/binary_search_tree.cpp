727937bfe6620438673b7b4ca803d1612d1faf75
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-05 14:24:40 +0530

    delete node

--- LEARNING/BST/binary_search_tree.cpp
+++ LEARNING/BST/binary_search_tree.cpp
@@ -41,33 +41,30 @@ class BST
     void inOrderTraversal(Node *temp)
     {
         if (temp == nullptr)
-        {
             return;
-        }
         inOrderTraversal(temp->left);
         cout << temp->value << "\t";
         inOrderTraversal(temp->right);
     }
+
     void preOrderTraversal(Node *temp)
     {
         if (temp == nullptr)
-        {
             return;
-        }
         cout << temp->value << "\t";
         preOrderTraversal(temp->left);
         preOrderTraversal(temp->right);
     }
+
     void postOrderTraversal(Node *temp)
     {
         if (temp == nullptr)
-        {
             return;
-        }
         postOrderTraversal(temp->left);
         postOrderTraversal(temp->right);
         cout << temp->value << "\t";
     }
+
     void levelOrderTraversal(Node *temp)
     {
         if (temp == nullptr)
@@ -98,18 +95,88 @@ class BST
             }
         }
     }
-    Node *searchTree(Node *temp, int value)
+
+    bool searchTree(Node *temp, int value)
     {
         if (temp == nullptr)
-            return nullptr;
+            return false;
 
         if (temp->value == value)
-            return temp;
+            return true;
 
-        if (value > temp->value)
+        if (value < temp->value) // corrected direction for search
             return searchTree(temp->left, value);
         else
             return searchTree(temp->right, value);
+    }
+
+    bool optimumSearch(Node *temp, int value)
+    {
+        while (temp != nullptr)
+        {
+            if (temp->value == value)
+                return true;
+
+            if (value < temp->value)
+                temp = temp->left;
+            else
+                temp = temp->right;
+        }
+        return false;
+    }
+
+    Node *findMin(Node *temp)
+    {
+        while (temp->left != nullptr)
+            temp = temp->left;
+        return temp;
+    }
+
+    Node *deleteNode(Node *temp, int value)
+    {
+        if (temp == nullptr)
+            return temp;
+
+        if (value < temp->value)
+            temp->left = deleteNode(temp->left, value);
+        else if (value > temp->value)
+            temp->right = deleteNode(temp->right, value);
+        else
+        {
+            // Node with only one child or no child
+            if (temp->left == nullptr and temp->right == nullptr)
+
+            {
+                delete temp;
+                return nullptr;
+            }
+            if (temp->left != nullptr and temp->right == nullptr)
+            {
+                Node *leftchild = temp->left;
+                delete temp;
+                return leftchild;
+            }
+            if (temp->right != nullptr and temp->left == nullptr)
+            {
+                Node *rightchild = temp->right;
+                delete temp;
+                return rightchild;
+            }
+
+            // Node with two children: Get the inorder successor
+            if (temp->right != nullptr and temp->left != nullptr)
+            {
+                Node *minNode = findMin(temp->right);
+                temp->value = minNode->value;
+                temp->right = deleteNode(temp->right, minNode->value);
+            }
+
+            if(temp->right and temp->left)
+            {
+                Node* maxNode=findMax)
+            }
+        }
+        return temp;
     }
 
 public:
@@ -128,27 +195,42 @@ public:
             root = BuildBST(root, value);
             cout << "Enter Element :: " << endl;
             cin >> value;
-        } // O(log n)
+        }
     }
+
     void InorderTraversal()
     {
         inOrderTraversal(root);
     }
+
     void PreOrderTraversal()
     {
         preOrderTraversal(root);
     }
+
     void PostOrderTraversal()
     {
         postOrderTraversal(root);
     }
+
     void LevelOrderDisplay()
     {
         levelOrderTraversal(root);
     }
+
     bool SearchNode(int value)
     {
-        return searchTree(root,value);
+        return searchTree(root, value);
+    }
+
+    bool OptimizedSearchNode(int value)
+    {
+        return optimumSearch(root, value);
+    }
+
+    void DeleteNode(int value)
+    {
+        root = deleteNode(root, value);
     }
 };
 
@@ -170,9 +252,20 @@ int main()
     cout << endl;
     cout << "PostOrder Traversal :: ";
     bst.PostOrderTraversal();
- cout<<boolalpha;
- cout<<endl;
- cout<<"10??"<<bst.SearchNode(10)<<endl;
- cout<<"432??"<<bst.SearchNode(432)<<endl;
+    cout << boolalpha;
+    cout << endl;
+    cout << "Search 10: " << bst.SearchNode(10) << endl;
+    cout << "Search 432: " << bst.SearchNode(432) << endl;
+    cout << endl;
+    cout << "Optimized Search 10: " << bst.OptimizedSearchNode(10) << endl;
+    cout << "Optimized Search 432: " << bst.OptimizedSearchNode(432) << endl;
+    cout << endl;
+
+    // Delete node example
+    cout << "Deleting node 10..." << endl;
+    bst.DeleteNode(10);
+    cout << "Level Order After Deletion:" << endl;
+    bst.LevelOrderDisplay();
+
     return EXIT_SUCCESS;
 }
