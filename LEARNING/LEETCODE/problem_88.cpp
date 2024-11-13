59c38a6fa4380a67b3d552d79243f59ccaa21466
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-13 08:23:03 +0530

    add solution for the problem 88

--- LEARNING/LEETCODE/problem_88.cpp
+++ LEARNING/LEETCODE/problem_88.cpp
@@ -23,3 +23,25 @@ public:
 
     }
 };
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i = m - 1; // Pointer for the end of nums1's initial elements
+//         int j = n - 1; // Pointer for the end of nums2
+//         int k = m + n - 1; // Pointer for the end of nums1's full capacity
+
+//         // Merge in reverse order, starting from the back
+//         while (i >= 0 && j >= 0) {
+//             if (nums1[i] > nums2[j]) {
+//                 nums1[k--] = nums1[i--];
+//             } else {
+//                 nums1[k--] = nums2[j--];
+//             }
+//         }
+
+//         // If there are remaining elements in nums2, copy them
+//         while (j >= 0) {
+//             nums1[k--] = nums2[j--];
+//         }
+//     }
+// };
