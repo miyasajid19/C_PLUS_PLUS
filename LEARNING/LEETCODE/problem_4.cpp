979b2b2bdd54351edf0315470b41c1273b0afbc9
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-16 08:51:14 +0530

    add solution for the fourth problem

--- LEARNING/LEETCODE/problem_4.cpp
+++ LEARNING/LEETCODE/problem_4.cpp
@@ -35,3 +35,45 @@ public:
             return (merged[n / 2 - 1] + merged[n / 2]) / 2.0;
     }
 };
+//############# the code should be as ######################---> time complexity should be as of log(m+n) but mine was n
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         // Ensure nums1 is the smaller array for binary search
+//         if (nums1.size() > nums2.size())
+//             return findMedianSortedArrays(nums2, nums1);
+
+//         int m = nums1.size();
+//         int n = nums2.size();
+//         int half_len = (m + n + 1) / 2;
+        
+//         int left = 0, right = m;
+//         while (left <= right) {
+//             int i = (left + right) / 2;
+//             int j = half_len - i;
+
+//             int nums1_left_max = (i == 0) ? INT_MIN : nums1[i - 1];
+//             int nums1_right_min = (i == m) ? INT_MAX : nums1[i];
+//             int nums2_left_max = (j == 0) ? INT_MIN : nums2[j - 1];
+//             int nums2_right_min = (j == n) ? INT_MAX : nums2[j];
+
+//             if (nums1_left_max <= nums2_right_min && nums2_left_max <= nums1_right_min) {
+//                 // Correct partition found
+//                 if ((m + n) % 2 == 1) {
+//                     return max(nums1_left_max, nums2_left_max);
+//                 } else {
+//                     return (max(nums1_left_max, nums2_left_max) + min(nums1_right_min, nums2_right_min)) / 2.0;
+//                 }
+//             } else if (nums1_left_max > nums2_right_min) {
+//                 // Move left in nums1
+//                 right = i - 1;
+//             } else {
+//                 // Move right in nums1
+//                 left = i + 1;
+//             }
+//         }
+
+//         throw invalid_argument("Input arrays are not sorted.");
+//     }
+// };
