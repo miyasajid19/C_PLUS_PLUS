a464aa964705b68b5d61fda9e93e2d82e8f482dd
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-17 19:37:00 +0530

    kth largest sum subarray approach 2

---
+++ LEARNING/HEAPS/kth_largest_sum_subarray_approach_2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class solution
+{
+    // Define a function to reset sumstore each time KthLargestSum is called
+    priority_queue<int, vector<int>, greater<int>> sumstore;
+
+public:
+    int KthLargestSum(vector<int> arr, int k)
+    {
+        // Clear the priority queue each time the function is called
+        sumstore = priority_queue<int, vector<int>, greater<int>>();
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            int sum = 0;
+            for (int j = i; j < arr.size(); j++)
+            {
+                sum += arr[j];
+                if (sumstore.size() < k)
+                {
+                    sumstore.push(sum);
+                }
+                else
+                {
+                    if (sum > sumstore.top())
+                    {
+                        sumstore.pop();
+                        sumstore.push(sum);
+                    }
+                }
+            }
+        }
+        return sumstore.top();
+    }
+};
+
+int main()
+{
+    vector<int> arr = {-3, -2, -1, 0, 1, 2, 3, 4};
+    solution soln;
+
+    // Iterate over k from 1 to arr.size(), printing the kth largest sum
+    for (int i = 1; i <= arr.size(); i++)
+    {
+        cout << "When k = " << i << ", the kth largest sum is " << soln.KthLargestSum(arr, i) << endl
+             << endl;
+    }
+    return EXIT_SUCCESS;
+}
